2262ca70bb9022dac4543cf339010ec2d7da0eb5
rallagas <80924362+rallagas@users.noreply.github.com>
2024-05-13 17:08:56 +0800

    Add files via upload
    Can now Checkout. and view order lists

--- db.php
+++ db.php
@@ -11,4 +11,21 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
+
+function gen_order_ref_number($len){
+    $alpha_num=array('A','B','C','D','E','F','G','H','I','J'
+                     ,'K','L','M','N','O','P','Q','R','S','T'
+                     ,'U','V','W','X','Y','Z','0','1','2','3'
+                     ,'4','5','6','7','8','9','0');
+    $key="";
+    for ($i = 0; $i <= $len; $i++){
+        if($i%2 == 0 && $i > 0){
+           $key .= $alpha_num[rand(0,25)];
+        }
+        else{
+             $key .= $alpha_num[rand(26,sizeof($alpha_num)-1)];
+        }
+     }
+    return $key;
+}
 ?>
---
+++ navigation.php
@@ -0,0 +1,6 @@
+<div class="btn-group ">
+    <a href="" class="btn btn-outline-light">Home</a>
+    <a href="" class="btn btn-outline-light">Shop</a>
+    <a href="" class="btn btn-outline-light">Contacts</a>
+    <a href="" class="btn btn-outline-light">About</a>
+</div>
